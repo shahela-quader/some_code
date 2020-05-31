361ad0b7cf5c4a6967793226ebc71bd164beae8b
Jenkins Automation Server <jenkins@actinemq2.example.com>
2020-05-31 18:01:23 -0400

    deleting lines

--- testscript.sh
+++ testscript.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "this script is from github"
-echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "this is complete"
-echo "this is complete"
 echo "this is complete"
